b368653ea564c1ea39b34068b0cdfb3ffc6d44f6
Maciej Podsiedlak <maciej.podsiedlak@gmail.com>
2021-10-29 20:28:25 +0200

    Layout and basic input masking for checkout

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,1 +1,6 @@
-import '../css/app.css'
+import '../css/app.css';
+import 'inputmask';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+window.Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,23 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        card: '275px',
-        checkout: '902px'
+        'card': '278px',
+        'checkout': '900px'
       },
-      backgroundColor: {
-        background: '#f2f2f2'
+      height: {
+        '13': '3.25rem'
       },
       boxShadow: {
         'solid-bottom': '0px 2px 0px 0px #D9D9D9',
       },
       outline: {
-        card: ['1px dashed #ededed', '-5px'],
+        'card': ['1px dashed #ededed', '-6px'],
       },
       borderColor: {
         'gray-250': '#ededed'
+      },
+      spacing: {
+        '4.5': '1.063rem'
       }
     }
   },
