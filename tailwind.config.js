1cbeda84a8a6780f17d8fbb4ba9bd166c4c9f76c
Maciej Podsiedlak <maciej.podsiedlak@gmail.com>
2021-10-30 12:44:06 +0200

    Icons for checkout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,27 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        'card': '278px',
-        'checkout': '900px'
+        card: '278px',
+        checkout: '900px'
       },
       height: {
-        '13': '3.25rem'
+        13: '3.25rem'
       },
       boxShadow: {
-        'solid-bottom': '0px 2px 0px 0px #D9D9D9',
+        'solid-gray-bottom': '0px 2px 0px 0px #D9D9D9',
+        'solid-green-bottom': '0px 2px 0px 0px #5db603'
       },
       outline: {
-        'card': ['1px dashed #ededed', '-6px'],
+        card: ['1px dashed #ededed', '-6px']
       },
       borderColor: {
         'gray-250': '#ededed'
       },
+      backgroundColor: {
+        green: '#6bd400'
+      },
       spacing: {
-        '4.5': '1.063rem'
+        4.5: '1.063rem'
       }
     }
   },
