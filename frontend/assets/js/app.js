7cd1d9224b994147136b8ad17cb9707450045d6d
Maciej Podsiedlak <maciej.podsiedlak@gmail.com>
2021-11-10 15:18:35 +0100

    CC expiration date validation improvement

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -6,3 +6,16 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 window.Alpine.start();
+
+// custom rule for "MM/YY" date format
+window.Iodine.addRule('monthYearAfterOrEqual', (value, param) => {
+  const todayDate = new Date();
+  const expirationDate = new Date();
+  const [month, year] = value.split('/');
+
+  expirationDate.setFullYear(`20${year}`, parseInt(month, 10) - 1, 1);
+
+  return expirationDate > todayDate;
+});
+
+window.Iodine.setErrorMessages({ ...window.Iodine.messages, monthYearAfterOrEqual: 'Please select a valid date' });
